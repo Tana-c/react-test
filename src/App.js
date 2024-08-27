33fa4aa3656d0b171530e868212e21a88cc206f5
Tana-c <herokoonchay@gmail.com>
2024-08-27 16:36:24 +0700

    new file:   public/cat.png 	modified:   public/index.html 	modified:   public/manifest.json 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/App.test.js 	new file:   src/Button.js 	new file:   src/Mycss.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+  const name = "TANA";
+
+  const employees = [
+    { name: "tana", email: "tana@ksu.ac.th", age: 21 },
+    { name: "boy", email: "tana@ksu.ac.th", age: 40 },
+    { name: "back", email: "tana@ksu.ac.th", age: 2 }
+  ]
+
+  console.log(employees);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+      <div className="button-container">
+        <h2>Count: {count}</h2>
+        <button onClick={incrementCount} className="large-button">+1</button>
+        <button onClick={decrementCount} className="large-button">-1</button>
+      </div>
+
+      <div className="App">
+        {employees.map((em, index) => (
+          <li key={index}> 
+            ชื่อพนักงาน: {em.name} อีเมล: {em.email} อายุ: {em.age} 
+          </li>
+        ))}
+        <h1>Hello {name}</h1>
+        <Button text="OK" />
+        <Button text="Cancel" />
+      </div>
+      <h2 className="test">Test</h2>
+    </>
+   );
 }
 
 export default App;
